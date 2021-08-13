3be3f8fac1a644b46d4b5959e744a40ea6166687
mohamad <mouhamad_mokh@hotmail.com>
2021-08-13 23:12:37 +0300

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -70,37 +70,35 @@ module.exports = {
                   "css-loader",
                   "sass-loader",
                 ],
-              },
+            },
 
-            /*{
-                test: /\.(png|jpe?g|gif|svg)$/,
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
                 use: [
                     {
-                        loader: "file-loader",
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: "images/",
-                        },
+                    loader: 'image-webpack-loader',
+                    options: {
+                        bypassOnDebug: true,
+                    mozjpeg: {
+                        progressive: true,
+                    },
+                    optipng: {
+                        enabled: false,
+                    },
+                    pngquant: {
+                        quality: [0.65, 0.90],
+                        speed: 4
+                    },
+                    gifsicle: {
+                        interlaced: false,
+                    },
+                    webp: {
+                        quality: 75
                     }
-                ]  
-            },*/
-
-            /*{
-                test: /\.(svg|eot|woff|woff2|ttf)$/,
-                use: 
-                [
-                    {
-                        loader: "file-loader",
-                        options:
-                        {
-                            name: '[name].[ext]',
-                            outputPath: "/fonts",
-                            esModule: false,
-                        }
                     }
-                    
-                ]
-            },*/
+                },
+                ],
+            }
         ]
     },
 
